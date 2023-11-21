9897e4c2916fd4a740a628bbb5c1d34d1af3ddbf
Blowsight71345 <idiot71345@gmail.com>
2023-11-21 22:10:53 +0300

    add main

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import telebot
+from telebot import types
+import random
 
+bot = telebot.TeleBot("Your token")
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Рандомное число"))
+    bot.send_message(message.chat.id, "Выберите действие", reply_markup=markup)
+
+@bot.message_handler(commands=["hello"])
+def hello(message):
+    bot.send_message(message.chat.id, "Привет")
+
+
+@bot.message_handler(commands=["random"])
+def randomize(message):
+    num = random.randint(1, 100)
+    bot.send_message(message.chat.id, str(num))
+
+
+bot.polling(none_stop=True)
